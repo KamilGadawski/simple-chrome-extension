e93a131c1bf7f56bd8a2b576679589293d592376
kamil.g <kamil.gadawski@albit.com.pl>
2021-06-23 13:59:07 +0200

    Added dynamic link creation

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const linkList = document.getElementById('link-list');
 
 saveBtn.addEventListener('click', () => {
 	myLeads.push(inputEl.value);
-    renderLink();
+	inputEl.value = '';
+	renderLink();
 });
 
-
 function renderLink() {
-    myLeads.forEach((item) => {
-        let liElement = document.createElement('li');
-    
-        liElement.textContent = item;
-        linkList.appendChild(liElement);
-    });
+	myLeads.forEach((item) => {
+		let liElement = document.createElement('li');
+		let createATag = document.createElement('a');
+
+		createATag.href = item;
+		createATag.target = '_blank';
+		createATag.innerText = item;
+
+		liElement.appendChild(createATag);
+		linkList.appendChild(liElement);
+
+		console.log(liElement);
+	});
 }
