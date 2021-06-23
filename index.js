ac38ecfad4c73761c4dc4057adc4b39dea45e36a
kamil.g <kamil.gadawski@albit.com.pl>
2021-06-23 14:51:59 +0200

    Added saving to localstorage

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ const saveBtn = document.getElementById('input-btn');
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const linkList = document.getElementById('link-list');
+let linksFromStorage = JSON.parse(localStorage.getItem('storageLinks'));
 
 saveBtn.addEventListener('click', () => {
 	myLeads.push(inputEl.value);
 	inputEl.value = '';
+
+    localStorage.setItem('storageLinks', JSON.stringify(myLeads))
 	renderLink();
 });
 
+
+if (linksFromStorage) {
+    myLeads = linksFromStorage;
+    renderLink();
+}
+
+console.log(linksFromStorage);
+
 function renderLink() {
 	myLeads.forEach((item) => {
-		let liElement = document.createElement('li');
+    let liElement = document.createElement('li');
 		let createATag = document.createElement('a');
 
 		createATag.href = item;
