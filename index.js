22ea9eeae6eb4dcde489935fb0d7bf9f33e159fd
kamil.g <kamil.gadawski@albit.com.pl>
2021-06-24 09:17:17 +0200

    Added detection of pressing Enter in input

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const saveBtn = document.getElementById('input-btn');
-let myLeads = [];
+const delBtn = document.getElementById('input-del-btn');
 const inputEl = document.getElementById('input-el');
 const linkList = document.getElementById('link-list');
 let linksFromStorage = JSON.parse(localStorage.getItem('storageLinks'));
 
+let myLeads = [];
+
 saveBtn.addEventListener('click', () => {
+	localstorageSave();
+});
+
+inputEl.addEventListener('keydown', (e) => {
+	if (e.code === 'Enter') {
+		localstorageSave();
+	}
+});
+
+delBtn.addEventListener('click', () => {
+	localStorage.clear();
+	location.reload();
+});
+
+if (linksFromStorage) {
+	myLeads = linksFromStorage;
+	renderLink();
+}
+
+function localstorageSave() {
 	myLeads.push(inputEl.value);
 	inputEl.value = '';
 
-    localStorage.setItem('storageLinks', JSON.stringify(myLeads))
+	localStorage.setItem('storageLinks', JSON.stringify(myLeads));
 	renderLink();
-});
-
-
-if (linksFromStorage) {
-    myLeads = linksFromStorage;
-    renderLink();
 }
-
-console.log(linksFromStorage);
 
 function renderLink() {
 	myLeads.forEach((item) => {
-    let liElement = document.createElement('li');
+		let liElement = document.createElement('li');
 		let createATag = document.createElement('a');
 
 		createATag.href = item;
@@ -31,7 +45,5 @@ function renderLink() {
 
 		liElement.appendChild(createATag);
 		linkList.appendChild(liElement);
-
-		console.log(liElement);
 	});
 }
